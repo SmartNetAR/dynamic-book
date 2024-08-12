1f72987970cea64d3bc9e7f9e4bfb5af04e4aef5
Leonardo Casales <leonardo@smartnet.com.ar>
2024-08-11 22:11:56 -0300

    updating put correct words list of words so they will be unordered

--- src/app/activity-data/activity-data.component.ts
+++ src/app/activity-data/activity-data.component.ts
@@ -11,14 +11,32 @@ import { WriteQuestionsExercise } from '../write-questions-exercise';
   styleUrl: './activity-data.component.css'
 })
 export class ActivityDataComponent {
-  @Input() activity: Activity | undefined;
+  words: string[] = [];
 
-  getUniqueWords(exercises: PutWordsExercise[] | WriteQuestionsExercise[]): string[] {
+  @Input()
+  set activity(activity: Activity | undefined) {
+    if (activity?.type === "put-correct-words") {
+      this.words = this.getUnorderedWords(this.getUniqueWords(activity.exercises));
+    }
+  }
+
+  private getUniqueWords(exercises: PutWordsExercise[] | WriteQuestionsExercise[]): string[] {
     const wordsSet = new Set<string>();
     exercises.forEach(exercise => {
       exercise.words.forEach(word => wordsSet.add(word))
     });
 
     return Array.from(wordsSet);
+  }
+
+  private getUnorderedWords(words: string[]): string[] {
+    const unorderedWords: string[] = [];
+    while (words.length > 0) {
+      const index = Math.floor(Math.random() * words.length);
+      unorderedWords.push(words[index]);
+      words.splice(index, 1);
+    }
+
+    return unorderedWords;
   }
 }
